c606deaab4fd832c38285ea3d4dd38cf30d783a9
nyankosan2828 <69426088+nyankosan2828@users.noreply.github.com>
2020-09-21 00:26:54 +0900

    validatesの実装とエラーメッセージの検証と実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,22 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do
+    vilidates :image
+    validates :name
+    validates :description
+    validates :price, numericality:
+    { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range' }
+  end
+
+  with_options numericality: { message: 'select' } do
+    validates :category_id
+    validates :product_status_id
+    validates :postage_payer_id
+    validates :prefecture_code_id
+    validates :handling_time_id
+  end 
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :product_status
