ffc4f8fe1b6d7dc1694b301edcfe32b72d73e5af
nyankosan2828 <69426088+nyankosan2828@users.noreply.github.com>
2020-09-20 14:03:16 +0900

    アイテムモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_150809) do
+ActiveRecord::Schema.define(version: 2020_09_20_040706) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.integer "price", null: false
+    t.integer "product_status_id", null: false
+    t.integer "postage_payer_id", null: false
+    t.integer "prefecture_code_id", null: false
+    t.integer "handling_time_id", null: false
+    t.integer "category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", default: "", null: false
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name", default: "", null: false
-    t.string "family_name", default: "", null: false
-    t.string "first_name_kana", default: "", null: false
-    t.string "family_name_kana", default: "", null: false
-    t.date "birthday", default: 0, null: false
+    t.string "first_name", null: false
+    t.string "family_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "family_name_kana", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_09_10_150809) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
