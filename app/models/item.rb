65e64dacbd3164e3ec6597dde6470b6be1021a97
nyankosan2828 <69426088+nyankosan2828@users.noreply.github.com>
2020-09-21 00:14:37 +0900

    active_storageの実装とアイテムリスト関数の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :product_status
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :prefecture_code
+  belongs_to_active_hash :handling_time
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items       
   
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
   NAME_REGEX_KANA = /\A[ァ-ヶー－]+\z/
