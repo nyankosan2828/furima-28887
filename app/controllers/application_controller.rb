2500e197480ea746a57255020a833e794d942178
nyankosan2828 <69426088+nyankosan2828@users.noreply.github.com>
2020-09-22 17:42:33 +0900

    コメントアウトの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,6 @@ class ApplicationController < ActionController::Base
     before_action :basic_auth
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    # def exist_item?
-    #   unless Item.find(params[:id])
-    #     redirect_to root_path
-    #   end
-    # end
-
     protected
   
     def configure_permitted_parameters
