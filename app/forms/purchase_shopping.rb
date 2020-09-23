ae62c464682709c4922ae553819408e70b587f38
nyankosan2828 <69426088+nyankosan2828@users.noreply.github.com>
2020-09-23 23:43:02 +0900

    テーブルカラムの追加及び「sold-out」の表示の有無

--- app/forms/purchase_shopping.rb
+++ app/forms/purchase_shopping.rb
@@ -5,6 +5,7 @@ class PurchaseShopping
   with_options presence: true do
     validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
     validates :city
+    validates :address
     validates :building_name
     validates :phone_number, length: { maximum: 11 }
   end
@@ -13,6 +14,6 @@ class PurchaseShopping
 
   def save
     Purchase.create(user_id: user_id, item_id: item_id)
-    ShoppingAddress.create(post_code: post_code, prefecture_code: prefecture_code, city: city, building_name: building_name, phone_number: phone_number, item_id: item_id)
+    ShoppingAddress.create(post_code: post_code, prefecture_code: prefecture_code, city: city, address: address, building_name: building_name, phone_number: phone_number, item_id: item_id)
   end
 end
