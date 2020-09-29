c42f2ab0be74f2aac1136ab2727e5d33efd492bf
nyankosan2828 <69426088+nyankosan2828@users.noreply.github.com>
2020-09-29 15:23:49 +0900

    ユーザー登録の正規表現修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,11 @@ class User < ApplicationRecord
   
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
   NAME_REGEX_KANA = /\A[ァ-ヶー－]+\z/
-  
+  PASSWORD_REGEX =  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
   with_options presence: true do
     validates :nickname
-    validates :password_confirmation, format: { with: /\A[a-z\d]{6,}+\z/i, message: 'is invalid. Input password.' }
+    validates :password_confirmation
     validates :first_name, format: { with: NAME_REGEX, message: 'is invalid. Input full-width characters.' }
     validates :family_name, format: { with: NAME_REGEX, message: 'is invalid. Input full-width characters.' }
     validates :first_name_kana, format: { with: NAME_REGEX_KANA, message: 'is invalid. Input full-width katakana characters.' }
@@ -21,6 +22,6 @@ class User < ApplicationRecord
   end
 
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'need to include @' }
-  validates :password, format: { with: /\A[a-z\d]{8,}+\z/i, message: 'is invalid. Input password.' }
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
 end
